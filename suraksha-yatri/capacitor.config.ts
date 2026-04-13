adaa16ad45b2dd7b0407f798ee9601fa7f2f10b3
Abhi13shek <abhisheksingh13dz@gmail.com>
2026-04-13 23:52:26 +0530

    Removed nested repo issue

---
+++ suraksha-yatri/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Suraksha Yatri',
+  webDir: 'dist'
+};
+
+export default config;
